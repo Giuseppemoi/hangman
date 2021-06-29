b4bddc9e131407b44c4eff1db56b1d5758b68f3f
GiuseppeMoi <giuseppemoi1989@gmail.com>
2021-06-29 13:56:07 +0200

    commit

--- script.js
+++ script.js
@@ -10,29 +10,108 @@
     ctx.lineTo(150, 70);
     ctx.stroke();
     
-    ctx.moveTo(50, 50);
-    ctx.lineTo(50, 200);
+    ctx.moveTo(50, 47);
+    ctx.lineTo(50, 250);
     ctx.stroke();
 
-    ctx.moveTo(20, 200);
-    ctx.lineTo(80, 200);
+    ctx.moveTo(20, 250);
+    ctx.lineTo(80, 250);
     ctx.stroke();
 
+    // // tete
+    // ctx.lineWidth="4";
+    // ctx.strokeStyle="#31528C";
+    // ctx.beginPath();
+    // ctx.arc(150, 85, 15, 0, 2*Math.PI);
+    // ctx.stroke();
 
+    // // body
+    // ctx.moveTo(150, 100);
+    // ctx.lineTo(150, 170);
+    // ctx.stroke();
 
+    // // bras gauche
+    // ctx.moveTo(150, 110);
+    // ctx.lineTo(170, 150);
+    // ctx.stroke();
 
+    // // bras droit
+    // ctx.moveTo(150, 110);
+    // ctx.lineTo(130, 150);
+    // ctx.stroke();
 
+    // // jambe gauche
+    // ctx.moveTo(150, 170);
+    // ctx.lineTo(170, 200);
+    // ctx.stroke();
 
+    // // jambe droite
+    // ctx.moveTo(150, 170);
+    // ctx.lineTo(130, 200);
+    // ctx.stroke();
 
+    // // zob
+    // ctx.moveTo(150, 170);
+    // ctx.lineTo(150, 190);
+    // ctx.stroke();
 
+    function hang(x){
+        switch (x) {
+            case 7:
+                // tete
+                ctx.lineWidth="4";
+                ctx.strokeStyle="#31528C";
+                ctx.beginPath();
+                ctx.arc(150, 85, 15, 0, 2*Math.PI);
+                ctx.stroke();
+                break;
+            case 6:
+                // body
+                ctx.moveTo(150, 100);
+                ctx.lineTo(150, 170);
+                ctx.stroke();
+                break;
+            case 5:
+                // bras gauche
+                ctx.moveTo(150, 110);
+                ctx.lineTo(170, 150);
+                ctx.stroke();
+                break;
+            case 4:
+                // bras droit
+                ctx.moveTo(150, 110);
+                ctx.lineTo(130, 150);
+                ctx.stroke();
+                break;
+            case 3:
+                // jambe gauche
+                ctx.moveTo(150, 170);
+                ctx.lineTo(170, 200);
+                ctx.stroke();
+                break;
+            case 2:
+                // jambe gauche
+                ctx.moveTo(150, 170);
+                ctx.lineTo(130, 200);
+                ctx.stroke();
+                break;
+            case 1:
+                // zob
+                ctx.moveTo(150, 170);
+                ctx.lineTo(150, 190);
+                ctx.stroke();
+                break;
 
+        }
+    }
 
+    //document.getElementById("before").insertBefore(`<h2 class="word" id="wordFind"></h2>`)
 
-
-    var word = ["maison", "voiture", "avion", "aaaaa", "malabarminion"]
+    var word = ["ANGLE","ARMOIRE","BANC","BUREAU","CABINET","CARREAU","CHAISE","CLASSE","CLEF","COIN","COULOIR","DOSSIER","EAU","ECOLE","ENTRER","ESCALIER","ETAGERE","EXTERIEUR","FENETRE","INTERIEUR","LAVABO","LIT","MARCHE","MATELAS","MATERNELLE","MEUBLE","MOUSSE","MUR","PELUCHE","PLACARD","PLAFOND","PORTE","POUBELLE","RADIATEUR","RAMPE","RIDEAU","ROBINET","SALLE","SALON","SERRURE","SERVIETTE","SIEGE","SIESTE","SILENCE","SOL","SOMMEIL","SONNETTE","SORTIE","TABLE","TABLEAU","TABOURET","TAPIS","TIROIR","TOILETTE","VITRE","ALLER","AMENER","APPORTER","APPUYER","ATTENDRE","BAILLER","COUCHER","DORMIR","ECLAIRER","EMMENER","EMPORTER","ENTRER","FERMER","FRAPPER","INSTALLER","LEVER","OUVRIR","PRESSER","RECHAUFFER","RESTER","SONNER","SORTIR","VENIR","ABSENT","ASSIS","BAS","HAUT","PRESENT","GAUCHE","DROITE","DEBOUT","DEDANS","DEHORS","FACE","LOIN","PRES","TARD","TOT","APRES","AVANT","CONTRE","DANS","DE","DERRIERE","DEVANT","DU","SOUS","SUR","CRAYON","STYLO","FEUTRE","MINE","GOMME","DESSIN","COLORIAGE","RAYURE","PEINTURE","PINCEAU","COULEUR","CRAIE","PAPIER","FEUILLE","CAHIER","CARNET","CARTON","CISEAUX","DECOUPAGE","PLIAGE","PLI","COLLE","AFFAIRE","BOITE","CASIER","CAISSE","TROUSSE","CARTABLE","JEU","JOUET","PION","DOMINO","PUZZLE","CUBE","PERLE","CHOSE","FORME","CARRE","ROND","PATE","MODELER","TAMPON","LIVRE","HISTOIRE","BIBLIOTHEQUE","IMAGE","ALBUM","TITRE","CONTE","DICTIONNAIRE","MAGAZINE","CATALOGUE","PAGE","LIGNE","MOT","ENVELOPPE","ETIQUETTE","CARTE","APPEL","AFFICHE","ALPHABET","APPAREIL","CAMESCOPE","CASSETTE","CHAINE","CHANSON","CHIFFRE","CONTRAIRE","DIFFERENCE","DOIGT","ECRAN","ECRITURE","FILM","FOIS","FOI","IDEE","INSTRUMENT","INTRUS","LETTRE","LISTE","MAGNETOSCOPE","MAIN","MICRO","MODELE","MUSIQUE","NOM","NOMBRE","ORCHESTRE","ORDINATEUR","PHOTO","POINT","POSTER","POUCE","PRENOM","QUESTION","RADIO","SENS","TAMBOUR","TELECOMMANDE","TELEPHONE","TELEVISION","TRAIT","TROMPETTE","VOIX","XYLOPHONE","ZERO","CHANTER","CHERCHER","CHOISIR","CHUCHOTER","COLLER","COLORIER","COMMENCER","COMPARER","COMPTER","CONSTRUIRE","CONTINUER","COPIER","COUPER","DECHIRER","DECOLLER","DECORER","DECOUPER","DEMOLIR","DESSINER","DIRE","DISCUTER","ECOUTER","ECRIRE","EFFACER","ENTENDRE","ENTOURER","ENVOYER","FAIRE","FINIR","FOUILLER","GOUTER","IMITER","LAISSER","LIRE","METTRE","MONTRER","OUVRIR","PARLER","PEINDRE","PLIER","POSER","PRENDRE","PREPARER","RANGER","RECITER","RECOMMENCER","REGARDER","REMETTRE","REPETER","REPONDRE","SENTIR","SOULIGNER","TAILLER","TENIR","TERMINER","TOUCHER","TRAVAILLER","TRIER","AMI","ATTENTION","CAMARADE","COLERE","COPAIN","COQUIN","DAME","DIRECTEUR","DIRECTRICE","DROIT","EFFORT","ELEVE","ENFANT","FATIGUE","FAUTE","FILLE","GARCON","GARDIEN","MADAME","MAITRE","MAITRESSE","MENSONGE","ORDRE","PERSONNE","RETARD","JOUEUR","SOURIRE","TRAVAIL","AIDER","DEFENDRE","DESOBEIR","DISTRIBUER","ECHANGER","EXPLIQUER","GRONDER","OBEIR","OBLIGER","PARTAGER","PRETER","PRIVER","PROMETTRE","PROGRES","PROGRESSER","PUNIR","QUITTER","RACONTER","EXPLIQUER","REFUSER","SEPARER","BLOND","BRUN","CALME","CURIEUX","DIFFERENT","DOUX","ENERVER","GENTIL","GRAND","HANDICAPE","INSEPARABLE","JALOUX","MOYEN","MUET","NOIR","NOUVEAU","PETIT","POLI","PROPRE","ROUX","SAGE","SALE","SERIEUX","SOURD","TRANQUILLE","ARROSOIR","ASSIETTE","BALLE","BATEAU","BOITE","BOUCHON","BOUTEILLE","BULLES","CANARD","CASSEROLE","CUILLERE","CUVETTE","DOUCHE","ENTONNOIR","GOUTTES","LITRE","MOULIN","PLUIE","POISSON","PONT","POT","ROUE","SAC","PLASTIQUE","SALADIER","SEAU","TABLIER","TASSE","TROUS","VERRE","AGITER","AMUSER","ARROSER","ATTRAPER","AVANCER","BAIGNER","BARBOTER","BOUCHER","BOUGER","DEBORDER","DOUCHER","ECLABOUSSER","ESSUYER","ENVOYER","COULER","PARTIR","FLOTTER","GONFLER","INONDER","JOUER","LAVER","MELANGER","MOUILLER","NAGER","PLEUVOIR","PLONGER","POUSSER","POUVOIR","PRESSER","RECEVOIR","REMPLIR","RENVERSER","SECHER","SERRER","SOUFFLER","TIRER","TOURNER","TREMPER","VERSER","VIDER","VOULOIR","AMUSANT","CHAUD","FROID","HUMIDE","INTERESSANT","MOUILLE","SEC","TRANSPARENT","MOITIE","AUTANT","BEAUCOUP","ENCORE","MOINS","PEU","PLUS","TROP","ANORAK","ARC","BAGAGE","BAGUETTE","BARBE","BONNET","BOTTE","BOUTON","BRETELLE","CAGOULE","CASQUE","CASQUETTE","CEINTURE","CHAPEAU","CHAUSSETTE","CHAUSSON","CHAUSSURE","CHEMISE","CIGARETTE","COL","COLLANT","COURONNE","CRAVATE","CULOTTE","ECHARPE","EPEE","FEE","FLECHE","FUSIL","GANT","HABIT","JEAN","JUPE","LACET","LAINE","LINGE","LUNETTES","MAGICIEN","MAGIE","MAILLOT","MANCHE","MANTEAU","MOUCHOIR","MOUFLE","NOEUD","PAIRE","PANTALON","PIED","POCHE","PRINCE","PYJAMA","REINE","ROBE","ROI","RUBAN","SEMELLE","SOLDAT","SOCIERE","TACHE","TAILLE","TALON","TISSU","TRICOT","UNIFORME","VALISE","VESTE","VETEMENT","CHANGER","CHAUSSER","COUVRIR","DEGUISER","DESHABILLER","ENLEVER","HABILLER","LACER","PORTER","RESSEMBLER","CLAIR","COURT","ETROIT","FONCE","JOLI","LARGE","LONG","MULTICOLORE","NU","USE","BIEN","MAL","MIEUX","PRESQUE","AIGUILLE","AMPOULE","AVION","BOIS","BOUT","BRICOLAGE","BRUIT","CABANE","CARTON","CLOU","COLLE","CROCHET","ELASTIQUE","FICELLE","FIL","MARIONNETTE","MARTEAU","METAL","METRE","MORCEAU","MOTEUR","OBJET","OUTIL","PEINTURE","PINCEAU","PLANCHE","PLATRE","SCIE","TOURNEVIS","VIS","VOITURE","ARRACHER","ATTACHER","CASSER","COUDRE","DETRUIRE","ECORCHER","ENFILER","ENFONCER","FABRIQUER","MESURER","PERCER","PINCER","REPARER","REUSSIR","SERVIR","TAPER","TROUER","TROUVER","ADROIT","DIFFICILE","DUR","FACILE","LISSE","MALADROIT","POINTU","TORDU","ACCIDENT","AEROPORT","CAMION","ENGIN","FEU","FREIN","FUSEE","GARAGE","GARE","GRUE","HELICOPTERE","MOTO","PANNE","PARKING","PILOTE","PNEU","QUAI","TRAIN","VIRAGE","VITESSE","VOYAGE","WAGON","ZIGZAG","ARRETER","ATTERRIR","BOUDER","CHARGER","CONDUIRE","DEMARRER","DISPARAITRE","DONNER","ECRASER","ENVOLER","GARDER","GARER","MANQUER","PARTIR","POSER","RECULER","ROULER","TENDRE","TRANSPORTER","VOLER","ABIME","ANCIEN","BLANC","BLEU","CASSE","CINQ","DERNIER","DEUX","DEUXIEME","DIX","GRIS","GROS","HUIT","JAUNE","MEME","NEUF","PAREIL","PREMIER","QUATRE","ROUGE","SEPT","SEUL","SIX","SOLIDE","TROIS","TROISIEME","UN","VERT","DESSUS","AUTOUR","VITE","VERS","ACROBATE","ARRET","ARRIERE","BARRE","BARREAU","BORD","BRAS","CERCEAU","CHAISE","CHEVILLE","CHUTE","COEUR","CORDE","CORPS","COTE","COU","COUDE","CUISSE","DANGER","DOIGTS","DOS","ECHASSES","ECHELLE","EPAULE","EQUIPE","ESCABEAU","FESSE","FILET","FOND","GENOU","GYMNASTIQUE","HANCHE","JAMBE","JEU","MAINS","MILIEU","MONTAGNE","MUR","ESCALADE","MUSCLE","NUMERO","ONGLE","PARCOURS","PAS","PASSERELLE","PENTE","PEUR","PIED","PLONGEOIR","POIGNET","POING","PONT","SIGNE","SINGE","POUTRE","EQUILIBRE","PRISE","RIVIERE","CROCODILE","ROULADE","PIROUETTE","SAUT","SERPENT","SPORT","SUIVANT","TETE","TOBOGGAN","TOUR","TRAMPOLINE","TUNNEL","VENTRE","ACCROCHER","APPUYER","ARRIVER","BAISSER","BALANCER","BONDIR","BOUSCULER","COGNER","COURIR","DANSER","DEPASSER","DESCENDRE","ECARTER","ESCALADER","GAGNER","GENER","GLISSER","GRIMPER","MARCHER","PATTES","DEBOUT","MONTER","MONTRER","PENCHER","PERCHER","PERDRE","RAMPER","RATER","REMPLACER","RESPIRER","RETOURNER","REVENIR","SAUTER","SOULEVER","SUIVRE","TOMBER","TRANSPIRER","TRAVERSER","DANGEUREUX","EPAIS","FORT","GROUPE","IMMOBILE","ROND","SERRE","SOUPLE","ENSEMBLE","ICI","JAMAIS","TOUJOURS","SOUVENT","BAGARRE","BALANCOIRE","BALLON","BANDE","BICYCLETTE","BILLE","CAGE","ECUREUIL","CERF","VOLANT","CHATEAU","COUP","COUR","COURSE","ECHASSE","FLAQUE","EAU","PAIX","PARDON","PARTIE","PEDALE","PELLE","POMPE","PREAU","RAQUETTE","RAYON","RECREATION","SABLE","SIFFLET","SIGNE","TAS","TRICYCLE","TUYAU","VELO","FILE","RANG","BAGARRER","BATTRE","CACHER","CRACHER","CREUSER","CRIER","DEGONFLER","DISPUTE","EMPECHER","GALOPER","HURLER","JONGLER","LANCER","PEDALER","PLAINDRE","PLEURER","POURSUIVRE","PROTEGER","SAIGNER","SALIR","SIFFLER","SURVEILLER","TRAINER","TROUVER","FOU","MECHANT"]
     var randomWord = word[Math.floor(Math.random()*word.length)];
     var underscore = "";
-    // console.log(randomWord)
+    var arrayLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    console.log(randomWord)
 
     for (let i = 0; i < randomWord.length; i++) {
             underscore += `<span id="span${i}">_ </span>`;
@@ -41,45 +120,55 @@
     document.getElementById("wordFind").innerHTML = underscore
 
     var arrayLetterSub = [];
-    var attempt = 10;
+    var attempt = 7;
     var goodAttempt = 0
+    var win = false
 
 
     document.addEventListener("keydown", (event)=> {
+        if (win) {
+            return 
+        }
 
         var letter = event.key;
+        letter = letter.toUpperCase()
         
-        if (arrayLetterSub.indexOf(letter) == -1) {
-            if (randomWord.indexOf(letter)!= -1){
-                var i = 0
-                while (i < randomWord.length) {
-                    if (randomWord[i] == letter) {
-                        document.getElementById(`span${i}`).innerHTML = letter
+        if (arrayLetters.indexOf(letter) >= 0) { 
+            if (arrayLetterSub.indexOf(letter) == -1) {
+                if (randomWord.indexOf(letter)!= -1){
+                    var i = 0
+                    while (i < randomWord.length) {
+                        if (randomWord[i] == letter) {
+                            document.getElementById(`span${i}`).innerHTML = letter
+                            goodAttempt++
+                        }
+                        i++
                     }
-                    i++
-                }
-                goodAttempt++
-                arrayLetterSub.push(letter)
-                document.getElementById("attempt").innerHTML = "Il vous reste " + attempt + " tentatives!"
-            }else{
-                attempt--
-                arrayLetterSub.push(letter)
-                document.getElementById("attempt").innerHTML = "Il vous reste " + attempt + " tentatives!"
-            }  
-        }else {
-            document.getElementById("attempt").innerHTML = "Vous avez deja proposé cette lettre!"
+                    arrayLetterSub.push(letter)
+                    document.getElementById("attempt").innerHTML = "Il vous reste " + attempt + " tentatives!"
+                }else{
+                    hang(attempt)
+                    attempt--
+                    arrayLetterSub.push(letter)
+                    document.getElementById("attempt").innerHTML = "Il vous reste " + attempt + " tentatives!"
+                }  
+            }else {
+                document.getElementById("attempt").innerHTML = "Vous avez deja proposé cette lettre!"
+            }
+    
+            document.getElementById("ltAlSub").innerText = arrayLetterSub
+    
+            if (attempt == 0) {
+                document.getElementById("attempt").innerHTML = "You loose!"
+                win = true
+            }
+    
+            if (goodAttempt == randomWord.length) {
+                document.getElementById("attempt").innerHTML = "You win!"
+                win = true
+            }
         }
-
-        document.getElementById("ltAlSub").innerText = arrayLetterSub
-
-        if (attempt == 0) {
-            document.getElementById("attempt").innerHTML = "You loose!"
-        }
-
-        if (goodAttempt == randomWord.length) {
-            document.getElementById("attempt").innerHTML = "You win!"
-        }
-    })
+    });
 })();
 
 
